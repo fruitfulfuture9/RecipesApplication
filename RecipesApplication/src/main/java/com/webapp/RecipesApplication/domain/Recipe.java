2cb02dc7162a51eaff48413dbfdb625d7d49352c
Vidhya Santhoshima <santhoshima.work@gmail.com>
2020-10-20 23:45:08 -0700

    Removed Whitespaces from code

--- RecipesApplication/src/main/java/com/webapp/RecipesApplication/domain/Recipe.java
+++ RecipesApplication/src/main/java/com/webapp/RecipesApplication/domain/Recipe.java
@@ -3,27 +3,16 @@ package com.webapp.RecipesApplication.domain;
 public class Recipe {
 	
 	 private Integer cookingMinutes;
-
      private Boolean dairyFree;
-
      private Boolean glutenFree;
-
      private String instructions;
-
      private Double preparationMinutes;
-
      private Double pricePerServing;
-
      private Integer readyInMinutes;
-
      private Integer servings;
-
      private Double spoonacularScore;
-
      private String title;
-
      private Boolean vegan;
-
      private Boolean vegetarian;
 
 	public Integer getCookingMinutes() {
